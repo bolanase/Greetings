aae930e3a357e5dc96e8560028180057b6fca5c1
Raghu Betina <raghu@starterleague.com>
2012-10-12 15:56:42 -0500

    VIEW: salutations

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ HowdyWorldApp::Application.routes.draw do
   get('/casual', { :controller => 'Greetings', :action => 'wazzup' })
   
   # ROUTE -> CONTROLLER -> ACTION -> VIEW
+
+  # :controller value must match controller filename, class name, view template folder name
+  # :action value must match controller method name, view template filename
+  
+  # No typos allowed!
+  
   # Instance variable: @my_variable
   # Embedded Ruby tag: <%= @my_variable %>
   
